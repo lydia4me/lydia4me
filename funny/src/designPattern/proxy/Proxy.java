5779d285cb23a4e8f63d4eaa73c5852bbb486b96
zhangjing <zhangjing1@nfschina.com>
2014-03-17 13:44:04 +0800

    proxy pattern

--- funny/src/designPattern/proxy/Proxy.java
+++ funny/src/designPattern/proxy/Proxy.java
@@ -4,8 +4,21 @@ public class Proxy implements Component {
 	private Component subject;
 	@Override
 	public void operate() {
-		this.subject = new Subject();
+		this.subject = new PrivateSubject();
 		this.subject.operate();
 	}
-
+	
+	private class PrivateSubject implements Component{
+		@Override
+		public void operate() {
+			System.out.println("operate in private subject");
+		}
+	}
+	
+	class ChildSubject implements Component{
+		@Override
+		public void operate() {
+			System.out.println("operate in child subject");
+		}
+	}
 }
