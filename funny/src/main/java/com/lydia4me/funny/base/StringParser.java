c9b71c968c2370357c8d89157d084855c0c01f49
lydia <lydia@lydia>
2014-03-25 23:16:09 +0800

    parseInt

--- funny/src/main/java/com/lydia4me/funny/base/StringParser.java
+++ funny/src/main/java/com/lydia4me/funny/base/StringParser.java
@@ -5,25 +5,44 @@ import java.util.regex.Pattern;
 
 public class StringParser {
 	public static void main(String[] args){
-		System.out.println(StringParser.parseInt("1235"));
+		System.out.println(StringParser.parseInt("12345"));
+		System.out.println(Integer.parseInt("3213213214214324123421312"));
 	}
 	
-	//bug很多，需改进
 	public static int parseInt(String str){
-		Pattern pattern = Pattern.compile("\\d+");
+		Pattern pattern = Pattern.compile("\\-?\\d+");
 		Matcher matcher = pattern.matcher(str);
 		if(!matcher.matches()){
-			throw new IllegalArgumentException();
+			throw new NumberFormatException();
 		}
 		int result = 0;
 		char[] chs = str.toCharArray();
+		int end = 0;
+		boolean negative = false;
+		if(chs[0] == '-'){
+			negative = true;
+			end = 1;
+		}
 		int multi = 1;
-		for(int i = chs.length - 1; i >= 0; i--){
+		int limit = Integer.MAX_VALUE/10;
+		if(negative){
+			limit = Integer.MIN_VALUE/10;
+		}
+		for(int i = chs.length - 1; i >= end; i--){
 			int bitInt = Character.digit(chs[i], 10);
 			if(i < chs.length - 1){
 				multi = multi * 10;
 			}
+			if(negative && -result < limit){
+				throw new NumberFormatException();
+			}
+			if(!negative && result > limit){
+				throw new NumberFormatException();
+			}
 			result += multi * bitInt;
+		}
+		if(negative){
+			return -result;
 		}
 		return result;
 	}
