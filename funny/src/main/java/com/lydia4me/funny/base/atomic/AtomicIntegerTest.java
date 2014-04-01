b48600b92a6294094c4b9f0674bcdffce7467077
lydia <lydia@lydia>
2014-04-02 00:17:16 +0800

    atomic

---
+++ funny/src/main/java/com/lydia4me/funny/base/atomic/AtomicIntegerTest.java
@@ -0,0 +1,41 @@
+package com.lydia4me.funny.base.atomic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest {
+	public static int normal = 0;
+	public static AtomicInteger atomicInt = new AtomicInteger(0);
+	public static void main(String[] args) {
+		List<Thread> threads = new ArrayList<Thread>();
+		for(int i = 0; i < 100; i ++){
+			Thread thread = new Thread(new Runnable(){
+				@Override
+				public void run() {
+					for(int j = 0; j < 100; j ++){
+						normal++;
+						atomicInt.incrementAndGet();
+						try {
+							Thread.sleep(10L);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			});
+			thread.start();
+			threads.add(thread);
+		}
+		for(Thread thread : threads){
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println(normal);
+		System.out.println(atomicInt.get());
+	}
+}
