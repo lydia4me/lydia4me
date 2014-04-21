3268d7ccd60d5ed6d87070011417c5406b409dfd
lydia <lydia@lydia>
2014-04-21 22:53:23 +0800

    min difference completed

--- funny/src/main/java/com/lydia4me/funny/algorithm/range/MinDifference.java
+++ funny/src/main/java/com/lydia4me/funny/algorithm/range/MinDifference.java
@@ -1,20 +1,48 @@
 package com.lydia4me.funny.algorithm.range;
-
+/**
+ * 求一个数组分割成差值最小的两个子集
+ *
+ */
 public class MinDifference {
 	public static void main(String[] args){
 		int[] input = {1,5,8,12,16,17,19,26,29};
-		
+		printMinDifference(input);
 	}
 	
 	public static void printMinDifference(int[] input){
-		int size = input.length;
 		int min = Integer.MAX_VALUE;
-		int range = 1 << size;
-		for(int i = range; i > 0; i--){
+		int minIndex = 0;
+		int range = 1 << input.length;
+		for(int i = 0; i < range; i++){
 			int sum = 0;
-			for(int j = 0; j < size; j ++){
-				
+			for(int j = 0; j < input.length; j ++){
+				int temp = 1 << j;
+				sum += (temp & i) == 0 ? input[j] : -input[j];
+			}
+			if(Math.abs(sum) < min){
+				min = Math.abs(sum);
+				minIndex = i;
 			}
 		}
+		System.out.println(min);
+		int sum1 = 0;
+		int sum2 = 0;
+		for(int j = 0; j < input.length; j ++){
+			int temp = 1 << j;
+			if((minIndex & temp) == 0){
+				System.out.print(input[j]+",");
+				sum1 += input[j];
+			}
+		}
+		System.out.println();
+		for(int j = 0; j < input.length; j ++){
+			int temp = 1 << j;
+			if((minIndex & temp) != 0){
+				System.out.print(input[j]+",");
+				sum2 += input[j];
+			}
+		}
+		System.out.println();
+		System.out.println(sum1+"   "+sum2);
 	}
 }
