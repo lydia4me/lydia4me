d6f94f78d10ce2fabadbb837c87b1069bdd777bc
zhangjing <zhangjing1@nfschina.com>
2014-04-21 21:01:57 +0800

    add minDifference; not completed

---
+++ funny/src/main/java/com/lydia4me/funny/algorithm/range/MinDifference.java
@@ -0,0 +1,20 @@
+package com.lydia4me.funny.algorithm.range;
+
+public class MinDifference {
+	public static void main(String[] args){
+		int[] input = {1,5,8,12,16,17,19,26,29};
+		
+	}
+	
+	public static void printMinDifference(int[] input){
+		int size = input.length;
+		int min = Integer.MAX_VALUE;
+		int range = 1 << size;
+		for(int i = range; i > 0; i--){
+			int sum = 0;
+			for(int j = 0; j < size; j ++){
+				
+			}
+		}
+	}
+}
