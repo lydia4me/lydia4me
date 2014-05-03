10bea222c2dd13384e09fe7965a181ef154cc786
lydia <jackey_010@163.com>
2014-05-03 18:05:53 +0800

    quick sort exercise

--- funny/src/main/java/com/lydia4me/funny/algorithm/sort/QuickSort.java
+++ funny/src/main/java/com/lydia4me/funny/algorithm/sort/QuickSort.java
@@ -4,41 +4,39 @@ public class QuickSort {
 	public static void main(String[] args) {
 		int[] arr = { 7, 3, 8, 4, 56, 78, 3, 2, 89, 4 };
 		quickSort(arr, 0, arr.length - 1);
-		for (int i : arr) {
-			System.out.println(i);
-		}
+        for (int i : arr){
+            System.out.print(i+",");
+        }
 	}
 
-	private static void quickSort(int[] arr, int start, int end) {
-		int i = start;
-		int j = end;
-		while(i < j){
-			while(i < j && arr[i] <= arr[j]){
-				i ++;
-			}
-			if(arr[i] > arr[j]){
-				swap(arr, i, j);
-			}
-			while(i < j && arr[i] <= arr[j]){
-				j --;
-			}
-			if(arr[i] > arr[j]){
-				swap(arr, i, j);
-			}
-		}
-		if(i - start > 1){
-			quickSort(arr, start, i - 1);
-		}
-		if(end - j > 1){
-			quickSort(arr, j + 1, end);
-		}
-	}
+    private static void quickSort(int[] arr, int start, int end) {
+        int i = start;
+        int j = end;
+        while(i < j){
+            while(i < j && arr[i] <= arr[j]){
+                i ++;
+            }
+            if(i < j && arr[i] > arr[j]){
+                swap(arr, i, j);
+            }
+            while(i < j && arr[i] <= arr[j]){
+                j --;
+            }
+            if(i < j && arr[i] > arr[j]){
+                swap(arr, i, j);
+            }
+        }
+        if(i - start > 0){
+            quickSort(arr, start, i - 1);
+        }
+        if(end - j > 0){
+            quickSort(arr, j + 1, end);
+        }
+    }
 
-	private static void swap(int[] arr, int i, int j) {
-		if(i >= 0 && i < arr.length && j >= 0 && j < arr.length){
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}
-	}
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
