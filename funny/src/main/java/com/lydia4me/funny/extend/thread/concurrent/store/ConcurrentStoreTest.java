8d3170f98e45ff66a150c0bed605c7f4476f99a1
lydia <jackey_010@163.com>
2014-05-06 07:30:52 +0800

    concurrent producer and customer. need fix

--- funny/src/main/java/com/lydia4me/funny/extend/thread/concurrent/store/ConcurrentStoreTest.java
+++ funny/src/main/java/com/lydia4me/funny/extend/thread/concurrent/store/ConcurrentStoreTest.java
@@ -1,68 +1,67 @@
 package com.lydia4me.funny.extend.thread.concurrent.store;
 
 import java.util.NoSuchElementException;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
-public class ConcurrentStoreTest implements Callable<String> {
-	private static ConcurrentLinkedQueue<String> msgQueue = new ConcurrentLinkedQueue<String>();
-	private int count = 0;
-	private String name;
-	
-	public ConcurrentStoreTest(String name) {
-		super();
-		this.name = name;
-	}
+public class ConcurrentStoreTest {
+	private static LinkedBlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(3);
 
-	@Override
-	public String call() throws Exception {
-		while(true){
-			try{
-				msgQueue.remove();
-				count ++;
-			}catch(NoSuchElementException e){
-				return "Thread:"+name+"; loaded "+count + "";
-			}
-			Thread.sleep(10L);
-		}
-	}
-	
 	public static void main(String[] args) {
-		Thread createThread = new Thread(new Runnable(){
-			@Override
-			public void run() {
-				for(int i = 0; i < 10000; i ++){
-					msgQueue.add(System.currentTimeMillis() + "");
-				}
-			}
-		});
-		createThread.start();
-		try {
-			createThread.join();
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-		
-		ExecutorService service = Executors.newFixedThreadPool(10);
-		CompletionService<String> completionService = new ExecutorCompletionService<String>(service);
-		for(int i = 0; i < 10; i ++){
-			completionService.submit(new ConcurrentStoreTest(i+""));
-		}
-		service.shutdown();
-		try {
-			for(int i = 0; i < 10; i ++){
-				System.out.println(completionService.take().get());
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
+		ExecutorService producerService = Executors.newFixedThreadPool(2);
+		CompletionService<String> completionService = new ExecutorCompletionService<String>(producerService);
+        completionService.submit(new Producer("1"));
+        completionService.submit(new Producer("2"));
+        producerService.shutdown();
+
+        ExecutorService customerService = Executors.newFixedThreadPool(2);
+        CompletionService<String> completionCustomerService = new ExecutorCompletionService<String>(customerService);
+        completionCustomerService.submit(new Customer("a"));
+        completionCustomerService.submit(new Customer("b"));
+        customerService.shutdown();
 	}
+
+    private static class Producer implements Callable<String>{
+        private String name;
+        public Producer(String name){
+            this.name = name;
+        }
+        @Override
+        public String call() {
+            while (true){
+                String msg = System.currentTimeMillis()+"";
+                msgQueue.add(msg);
+                System.out.println("Producer "+name+" : add "+msg);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class Customer implements Callable<String>{
+        private String name;
+        public Customer(String name){
+            this.name = name;
+        }
+        @Override
+        public String call() {
+            while (true){
+                String msg = null;
+                try {
+                    msg = msgQueue.poll(600, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    System.out.println("木有等到新的产品");
+                }
+                System.out.println("Customer "+name+" : poll "+msg);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
